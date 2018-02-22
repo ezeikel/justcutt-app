5a2371f95bf36c0bfe7c28f85fd5263e2e0d1f06
Ezeikel Pemberton <ezeikelpemberton@gmail.com>
2018-02-22 21:51:29 +0000

    App broke. Fresh install.

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default class App extends Component {
+export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Just Cutt</Text>
+        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Changes you make will automatically reload.</Text>
+        <Text>Shake your phone to open the developer menu.</Text>
       </View>
     );
   }
