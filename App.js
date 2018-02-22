42723ebf9fc9e00e8796582ac448b0b5a7c8d631
Ezeikel Pemberton <ezeikelpemberton@gmail.com>
2018-02-22 23:26:55 +0000

    Added styled-components to project.

--- App.js
+++ App.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
+import styled from 'styled-components';
+
+const StyledView = styled.View`
+  background-color: papayawhip;
+`;
+
+const StyledText = styled.Text`
+  color: palevioletred;
+  font-size: 54px;
+  font-family: Avenir;
+`;
 
 class App extends Component {
   render() {
     return (
-      <View>
-        <Text>Just Cutt</Text>
-      </View>
+      <StyledView>
+        <StyledText>{`Just Cutt`.toUpperCase()}</StyledText>
+      </StyledView>
     );
   }
 }
