8347d78d6aaac908712e83275c9cff5eb2f1e45b
Ezeikel Pemberton <ezeikelpemberton@gmail.com>
2018-05-13 19:36:00 +0100

    Pulling data from api into list.

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, FlatList } from 'react-native';
 import styled from 'styled-components';
 
 const StyledView = styled.View`
@@ -13,10 +13,41 @@ const StyledText = styled.Text`
 `;
 
 class App extends Component {
+  state = {
+    shops: []
+  }
+
+  async componentDidMount() {
+    const query = {
+      query: '{ shops { _id, name, slug, photo, location { coordinates, address }, tags, ratings } }'
+    };
+
+    const response = await fetch('http://justcutt.com/api', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(query)
+    });
+    const json = await response.json();
+
+    const { shops } = json.data;
+
+    this.setState({
+      shops
+    })
+  }
+
   render() {
     return (
       <StyledView>
         <StyledText>{`Just Cutt`.toUpperCase()}</StyledText>
+        <FlatList
+          data={this.state.shops}
+          renderItem={
+            ({item}) => <Text>{item.name}</Text>
+          }
+          // react native is wierd with the key prop
+          keyExtractor={(item, index) => index}
+        />
       </StyledView>
     );
   }
